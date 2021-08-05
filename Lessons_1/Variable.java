7e86db953ea2a7d4ff0c17404efd9966b12b19ef
alesya_l <alesya_l@holdingbp.ru>
2021-08-05 18:30:27 +0300

    implement home work_1

---
+++ Lessons_1/Variable.java
@@ -0,0 +1,35 @@
+public class Variable {
+    
+    public static void main (String[] args) {
+
+    /*
+        Примитивные типы данных
+        byte 
+        short
+        int
+        long
+        float
+        double
+        char
+        boolean
+
+
+    */
+        byte yadra = 8;
+        short hdd = 512;
+        int graphic = 16;
+        long z = 32000;
+        float p1 = 5.1f;
+        double buling = 3.1434234;
+        char feat = 'h';
+        boolean yadra1 = true;
+
+        System.out.println(yadra);
+        System.out.println(hdd);
+        System.out.println(graphic);
+        System.out.println(z);
+        System.out.println(p1);
+        System.out.println(buling);
+
+    }
+}
